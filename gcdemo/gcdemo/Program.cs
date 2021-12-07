a0817805537e82c1ed948518a06aa9e8093806e2
nps007-trainer <83720044+nps007-trainer@users.noreply.github.com>
2021-12-07 15:20:01 +0530

    source file of gcdemo

---
+++ gcdemo/gcdemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gcdemo
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> obj = new List<string>() { "Nandini", "S" };
+            Console.WriteLine(System.GC.GetGeneration(obj));
+            System.GC.Collect();
+            Console.WriteLine(System.GC.GetGeneration(obj));
+            System.GC.Collect();
+            Console.WriteLine(System.GC.GetGeneration(obj));
+
+            Console.ReadLine();
+
+        }
+    }
+}
