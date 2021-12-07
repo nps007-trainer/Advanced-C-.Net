f40a705122d95e845621de7fb0e3ec7683be4cf5
nps007-trainer <83720044+nps007-trainer@users.noreply.github.com>
2021-12-07 15:20:40 +0530

    source file of memory management

---
+++ memorymanagment/memorymanagment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memorymanagment
+{
+    
+    class Calculator
+    {
+        public int addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      Calculator calc=new Calculator();
+            Console.WriteLine(calc.addition(2,3));
+
+            Console.ReadLine();
+        }
+    }
+}
